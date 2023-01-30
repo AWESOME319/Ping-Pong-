a9c06317489d723f8cb87b7c207c0278aff1b49e
Whatever <83673925+AWESOME319@users.noreply.github.com>
2023-01-29 19:46:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 /*created by prashant shukla */
-
+video=""
 var paddle2 =10,paddle1=10;
 
 var paddle1X = 10,paddle1Height = 110;
@@ -23,13 +23,21 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+video.size(700, 600);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
 
 
 function draw(){
 
  background(0); 
-
+ image(video, 0, 0, 700, 600);
  fill("black");
  stroke("black");
  rect(680,0,20,700);
