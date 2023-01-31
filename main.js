4fd5867543207aa85d5b11ca13f20663bf07b01e
Whatever <83673925+AWESOME319@users.noreply.github.com>
2023-01-31 16:02:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
 /*created by prashant shukla */
 video=""
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
 var paddle2 =10,paddle1=10;
 
 var paddle1X = 10,paddle1Height = 110;
@@ -23,16 +26,29 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
-canvas.parent('canvas');
-
-video = createCapture(VIDEO);
-video.size(700, 600);
-video.hide();
-
-poseNet = ml5.poseNet(video, modelLoaded);
-poseNet.on('pose', gotPoses);
-}
-
+  canvas.parent('canvas');
+  
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+  }
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+  
+      rightWristY = results[0].pose.rightWrist.y;
+      rightWristX = results[0].pose.rightWrist.x;
+      scoreRightWrist =  results[0].pose.keypoints[10].score;
+      console.log(scoreRightWrist);
+    }
+  }
 
 function draw(){
 
@@ -42,9 +58,13 @@ function draw(){
  stroke("black");
  rect(680,0,20,700);
 
- fill("black");
- stroke("black");
- rect(0,0,20,700);
+
+ if(scoreRightWrist > 0.2)
+  {
+    fill("red");
+    stroke("red");
+    circle(rightWristX, rightWristY, 30);
+  }
  
    //funtion paddleInCanvas call 
    paddleInCanvas();
